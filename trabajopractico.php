9bc599bfbdb38cc84ec737471c1bc14ae7338b9d
Paola <ritaovando@gmail.com>
2021-08-10 07:25:48 -0300

    subiendo tp

---
+++ trabajopractico.php
@@ -0,0 +1,9 @@
+<?php
+echo "Nmbre: Rita Paola <BR>";
+echo"Apelido: Ovando <br>";
+echo"Edad: 39 <br>";
+echo"Hobbie: Tejer <br>";
+echo"Editor de Codigo Preferido: Visual Studio <br>";
+echo"Sitema Operativo que utilizo: Windows <br>";
+
+?>
